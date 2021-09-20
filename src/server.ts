4c580b3246fc243faa426052e36485796cfbf581
Lucas R. Hawthorne Dalbem <52721182+LucasDalben@users.noreply.github.com>
2021-09-20 20:59:19 -0300

    adding client and admin on websocket and html/io access

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,5 @@
-import express from 'express';
-import { createServer } from "http";
-import { Server, Socket } from 'socket.io'
-
-import "./database";
-import { routes } from './routes';
-
-const app = express();
-
-const http = createServer(app);
-const io = new Server(http);
-
-io.on("connection", (socket: Socket) => {
-    console.log("Socket connected", socket.id)
-})
-
-app.use(express.json());
-
-app.use(routes);
+import {  http } from "./http"
+import "./websocket/client"
 
 http.listen('3333', () => {
     console.log("Sir! The server is running on port 3333!")
